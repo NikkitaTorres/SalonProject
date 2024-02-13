b9472bdae80d38b4a0612f6797cf16c1a3f5e56e
NikkitaTorres <nikkitatorres@yahoo.com>
2024-02-13 10:58:24 -0800

    setup models, can launch home page

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using MySqlConnector;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public string Name { get; set; }
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
